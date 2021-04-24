e374a9caa3f6fa24b0578f88b971fca10a14ddc8
Rigel Kent <sendmemail@rigelk.eu>
2021-04-24 14:19:46 +0200

    changement de l'api semaine

--- api_urls.py
+++ api_urls.py
@@ -2,16 +2,16 @@ from django.urls import include, path
 from rest_framework import routers
 
 from vaccin.views import VaccinViewSet
-from cles_region.views import ClesRegionViewSet
+from cles_region.views import ClesRegionViewSet, ClesRegionsParSemaine
 from cles_departement.views import ClesDepartementViewSet, ClesDepartementsParSemaine
 from centre.views import CentreAmbulatoireViewSet
 
 router = routers.DefaultRouter()
 router.register(r'centres', CentreAmbulatoireViewSet, basename='centre')
 router.register(r'vaccins', VaccinViewSet, basename='vaccin')
-router.register(r'cdepartements', ClesDepartementViewSet, basename='departement')
-router.register(r'semaine/(?P<semaine>\d+)', ClesDepartementsParSemaine, basename='departement par semaine')
-#router.register(r'semaine/(?P<semaine>\d+)', ClesDepartementsParSemaine, basename='departement par semaine')
-router.register(r'cregions', ClesRegionViewSet, basename='region')
+router.register(r'departements', ClesDepartementViewSet, basename='departement')
+router.register(r'semaine/(?P<semaine>\d+)/departements', ClesDepartementsParSemaine, basename='departement par semaine')
+router.register(r'regions', ClesRegionViewSet, basename='region')
+router.register(r'semaine/(?P<semaine>\d+)/regions', ClesRegionsParSemaine, basename='region par semaine')
 
 urlpatterns = router.urls
