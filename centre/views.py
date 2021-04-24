a9938c1f1b7dd026a22a6e3aa679f3021a8a41f7
Rigel Kent <sendmemail@rigelk.eu>
2021-04-24 15:33:21 +0200

    allow searching centres

--- centre/views.py
+++ centre/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import CentreAmbulatoireSerializer
 from .models import CentreAmbulatoire
@@ -7,3 +10,9 @@ from .models import CentreAmbulatoire
 class CentreAmbulatoireViewSet(viewsets.ModelViewSet):
     queryset = CentreAmbulatoire.objects.all()
     serializer_class = CentreAmbulatoireSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('^code_postal', '=gid', 'nom')
+    filter_fields = ('gid', 'nom', 'capacite', 'code_postal')
+    ordering_fields = ('gid', 'nom', 'capacite', 'code_postal')
+    ordering = ('code_postal')
